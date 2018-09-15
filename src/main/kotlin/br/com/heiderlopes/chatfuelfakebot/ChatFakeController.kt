4abdd5b8df78fe9d96e22920813f08ebdaa8d158
Heider Lopes <heider.lopes@b2wdigital.com>
2018-09-15 13:42:25 -0300

    Passagem de parametro

--- src/main/kotlin/br/com/heiderlopes/chatfuelfakebot/ChatFakeController.kt
+++ src/main/kotlin/br/com/heiderlopes/chatfuelfakebot/ChatFakeController.kt
@@ -1,6 +1,7 @@
 package br.com.heiderlopes.chatfuelfakebot
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,6 +10,14 @@ class ChatFakeController {
     @RequestMapping("/saudacao")
     fun getSaudacao(): ChatfulResponse {
         val valores = mapOf("mensagemSaudacao" to "Oi vindo da api")
+        return ChatfulResponse(valores)
+    }
+
+    @RequestMapping("/saudacao2")
+    fun getSaudacao2(
+            @RequestParam("nome") nome: String
+    ): ChatfulResponse {
+        val valores = mapOf("mensagemSaudacao" to "Oi $nome")
         return ChatfulResponse(valores)
     }
 
